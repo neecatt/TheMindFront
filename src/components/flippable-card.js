75247fd0ddd574c4ecfbe8da2877a5dd2bc79657
Olli <58643307+R4M5E5@users.noreply.github.com>
2022-08-20 21:11:20 +0200

    Add basic card component

--- src/components/flippable-card.js
+++ src/components/flippable-card.js
@@ -1,9 +1,10 @@
 import './flippable-card.css';
+import Card from './card/card';
 
 function FlippableCard() {
     return(
         <div className="flippable-card-container">
-
+            <Card/>
         </div>
     );
 }

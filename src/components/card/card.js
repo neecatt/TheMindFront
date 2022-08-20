082b62720e377d434fa35d1b2fd885ee2e284444
Olli <58643307+R4M5E5@users.noreply.github.com>
2022-08-20 22:03:49 +0200

    Add basic front and back side to the card component

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,11 +1,12 @@
-import './card.css';
+import "./card.css";
 
 function Card() {
-    return(
-        <div className='card'>
-
-        </div>
-    );
+  return (
+    <div className="card">
+      <div className="card-back">Back</div>
+      <div className="card-front">Front</div>
+    </div>
+  );
 }
 
 export default Card;

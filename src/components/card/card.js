670981ee691fc5a358dec9bd754829db5f1ac251
Olli <58643307+R4M5E5@users.noreply.github.com>
2022-08-21 15:50:04 +0200

    Add react transition group and on click handler for card component

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,8 +1,8 @@
 import "./card.css";
 
-function Card() {
+function Card({onClick}) {
   return (
-    <div className="card">
+    <div className="card" onClick={onClick}>
       <div className="card-back">Back</div>
       <div className="card-front">Front</div>
     </div>
--- src/components/flippable-card.js
+++ src/components/flippable-card.js
@@ -1,10 +1,21 @@
 import './flippable-card.css';
 import Card from './card/card';
+import {CSSTransition} from 'react-transition-group';
+import {useState} from 'react';
 
 function FlippableCard() {
+    const [showFront, setShowFront] = useState(true);
     return(
         <div className="flippable-card-container">
-            <Card/>
+            <CSSTransition
+                in={showFront}
+                timeout={300}
+                classNames='flip'
+            >
+                <Card onClick={() => {
+                    setShowFront((v) => !v);
+                }}/>
+            </CSSTransition>
         </div>
     );
 }
